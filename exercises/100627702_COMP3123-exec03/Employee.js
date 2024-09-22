a0f7a56f337fa8fa35a82d538c859c5ea6bc6c06
Kailie Field <kailie.field@gmail.com>
2024-09-22 18:41:25 -0400

    formatting changes, console.log added for employee data

--- exercises/100627702_COMP3123-exec03/Employee.js
+++ exercises/100627702_COMP3123-exec03/Employee.js
@@ -1,13 +1,15 @@
 //TODO - Create Employee Module here and export to use in index.js
 
-// using req.method
-
 let employees = [
     {id: 1, firstName: "Pritesh", lastName: "Patel", email: "pritesh@gmail.com", Salary:5000},
     {id: 2, firstName: "Krish", lastName: "Lee", email: "krish@gmail.com", Salary:4000},
     {id: 3, firstName: "Racks", lastName: "Jacson", email: "racks@gmail.com", Salary:5500},
     {id: 4, firstName: "Denial", lastName: "Roast", email: "denial@gmail.com", Salary:9000}
 ]
+
+// Display to console
+for(const [key, value] of Object.entries(employees)) {console.log(key, "ID: " + value.id + " " + "First Name: " + " " + value.firstName + 
+                                                      "Last Name: " + " " + value.lastName + "Email: " + " " + value.email + "Salary: " + " " + value.Salary);}
 
 // Exporting for index.js
 module.exports = employees
--- exercises/100627702_COMP3123-exec03/index.js
+++ exercises/100627702_COMP3123-exec03/index.js
@@ -4,6 +4,7 @@ var http = require("http");
 
 //TODO - Use Employee Module here
 const employees = require('./Employee');
+
 console.log("Lab 03 -  NodeJs");
 
 //TODO - Fix any errors you found working with lab exercise
@@ -14,7 +15,7 @@ const port = process.env.PORT || 8081
 //Create Web Server using CORE API
 const server = http.createServer((req, res) => {
     if (req.method !== 'GET') {
-        res.writeHead(405, {'Content-Type' : 'application/json'});
+        res.writeHead(405, {'Content-Type' : 'application/json'}); // to accommodate JSON formatting easier
         res.end(`{"error": "${http.STATUS_CODES[405]}"}`)
         return;
     }
@@ -24,23 +25,25 @@ const server = http.createServer((req, res) => {
 
             // FORMATTING DOCUMENT FOR MY EASE OF UNDERSTANDING AND USER INTERACTION
             res.end(`
+                
                 <!DOCTYPE html>
                 <html>
                     <head>
                         <title> Lab Exercise 03 || COMP 3123 || KAILIE FIELD || 100627702 </title>
                         <style>
                         body {font-family: Courier New,Courier,Lucida Sans Typewriter, Lucida Typewriter, monospace; }
-                        h1 { color: #2x3e50; }
-                        p { font-size: 14px; }
+                        h1, h3 { color: #5fdb0d; }
+                        p, ul { font-size: 14px; color: #5fdb0d; }
                         </style>
                     </head>
                     <body>
-                    <h1> Welcome to Lab Exercise 03 </h1>
-                    <p> Explore </p>
+                    <h1> Welcome to Lab Exercise 03 || COMP 3123 || KAILIE FIELD || 100627702 </h1> 
+                    <h3>It's best you view this in Dark Mode :)</h3>
+                    <p> Explore Your Options</p>
                     <ul>
-                    <li><a href = "/employee">/employee</a> - All Employees</li>
-                    <li><a href = "/employee/names">/employee/names</a> - Employee Names</li>
-                    <li><a href = "/employee/totalsalary">/employee/totalsalary</a> - Total Salary of all Employees</li>
+                    <li>ALL EMPLOYEES --- <a href = "/employee">/employee</a></li>
+                    <li>EMPLOYEE NAMES --- <a href = "/employee/names">/employee/names</a></li>
+                    <li>TOTAL SALARY --- <a href = "/employee/totalsalary">/employee/totalsalary</a></li>
                     </ul>
                     </body>
                 </html>
@@ -48,36 +51,42 @@ const server = http.createServer((req, res) => {
             return;
         }
 
+        // if http://127.0.0.1:8081/employee
         if (req.url === '/employee') {
+
             //TODO - Display all details for employees in JSON format
-            res.writeHead(200, {'Content-Type' : 'application/json'});
-            res.end(JSON.stringify(employees, null, 2)); //JSON formatting
+            res.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
+            res.end(JSON.stringify(employees, null, 2)); //JSON formatting converting Javascript objects into Strings (value, replacer, space)
             return;
         }
-
+        // if http://127.0.0.1:8081/employee/names
         if (req.url === '/employee/names') {
+
             //TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
             //e.g. [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
-
         const employeeDetails = employees
             .map(employee => `${employee.firstName} ${employee.lastName}`)
             .sort();
-        res.writeHead(200, {'Content-Type' : 'application/json'});
-        res.end(JSON.stringify(employeeDetails, null, 2)); //JSON formatting
+        res.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
+        res.end(JSON.stringify(employeeDetails, null, 2)); //JSON formatting converting Javascript objects into Strings (value, replacer, space)
         return;
 
         }
-
+        // if http://127.0.0.1:8081/employee/totalsalary
         if (req.url === '/employee/totalsalary') {
+
             //TODO - Display Sum of all employees salary in given JSON format 
             //e.g. { "total_salary" : 100 }  
-        const total_salary = employees.reduce((acc, employee) => acc + employee.Salary, 0);
-        res.writeHead(200, {'Content-Type' : 'application/json'});
-        res.end(JSON.stringify({ total_salary }, null, 2)); //JSON formatting
+        const total_salary = employees.reduce((acc, employee) => acc + employee.Salary, 0); // reduce(accumulator, employee) 
+        res.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
+        res.end(JSON.stringify({ total_salary }, null, 2)); //JSON formatting converting Javascript objects into Strings (value, replacer, space)
         return;
+
     }
-    res.writeHead(404, {'Content-Type' : 'application/json'});
+
+    res.writeHead(404, {'Content-Type' : 'application/json; charset=utf-8'});
     res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+
     }
 )
 
