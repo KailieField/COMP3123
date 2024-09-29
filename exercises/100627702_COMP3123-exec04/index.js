4c49688696de6333a0f04b2f14d8a39f50102ba6
Kailie Field <kailie.field@gmail.com>
2024-09-29 16:42:16 -0400

    adding 100627702_COMP3123-exec04 with node modules, choosing to deselevet node_modules is not an option

---
+++ exercises/100627702_COMP3123-exec04/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const SERVER_PORT = process.env.port || 3000;
+
+
+// http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// http://localhost:3000/
+app. post('/', (req, res)=>{
+    res.send('POST - Hello World');
+})
+
+//Listen to the server
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+})
